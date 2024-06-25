148c4fde27f1e8f532d4e9e73878bdff17b4f314
RadomirOprischenko <164899846+RadomirOprischenko@users.noreply.github.com>
2024-06-25 15:56:39 +0200

    оооооооооооооокккккккккккккккк

---
+++ Assets/скрипты/реплика Hi.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class репликаHi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()    {
+       Debug.Log("HHHHiiii"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+      
+        
+      
+    }
+}
